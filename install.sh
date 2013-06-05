2110a6f8f0a33f5d6fd74b7dd06efba5dff71b1f
Lukas Senger <lukas@fridolin.com>
2013-06-05 16:57:37 +0200

    add rudimentary documentation

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install more recent version of node than in repository
+wget http://nodejs.org/dist/v0.10.7/node-v0.10.7-linux-arm-pi.tar.gz
+tar xf node-v0.10.7-linux-arm-pi.tar.gz
+sudo mkdir /opt/node
+sudo cp -r node-v0.10.7-linux-arm-pi/* /opt/node
+sudo sed -i '/^export PATH$/ i\NODE_JS_HOME="/opt/node"
+PATH="$PATH:$NODE_JS_HOME/bin"
+' /etc/profile
+
+# install deps
+cd pivid/node
+/opt/node/bin/npm install express
+/opt/node/bin/npm install jquery
