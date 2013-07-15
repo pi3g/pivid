1a8bb8a3f0d502686125414336d1d8cf1870e4c8
Lukas Senger <lukas@fridolin.com>
2013-07-15 20:32:43 +0000

    fix X permission issues

--- node/playcommand.sh
+++ node/playcommand.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 vid=$1
+display_user="$(who | grep ':0.0' | cut -d ' ' -f 1)"
 
+export XAUTHORITY=/home/$display_user/.Xauthority
 export DISPLAY=:0
 lxterminal -e "omxplayer $vid" && xrefresh
