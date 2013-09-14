ed94491aa2d040a7829c6aca05bc96f45984c539
Lukas Senger <ls@pi3g.com>
2013-09-14 20:00:38 +0000

    make display acquisition more robust

--- node/playcommand.sh
+++ node/playcommand.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
 vid=$1
-display_user="$(who | grep ':0.0' | cut -d ' ' -f 1)"
+export DISPLAY=:0.0
 
-export XAUTHORITY=/home/$display_user/.Xauthority
-export DISPLAY=:0
+# this function gets the display owner from console kit via dbus
+# it's needed sometimes because lightdm doesn't properly register logins
+ck_display_user() {
+	session="$(dbus-send --system --type=method_call --print-reply=literal --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Seat1 org.freedesktop.ConsoleKit.Seat.GetActiveSession)"
+	ruid="$(dbus-send --system --type=method_call --print-reply=literal --dest=org.freedesktop.ConsoleKit $session org.freedesktop.ConsoleKit.Session.GetUser)"
+	user="$(getent passwd ${ruid:10} | cut -d: -f1)"
+	echo $user
+}
+
+# get the display owner and permission to use it
+display_user="$(who | grep ':0.0' | cut -d ' ' -f 1)"
+if [ -z "$display_user" ]
+then
+	display_user="$(ck_display_user)"
+fi
+if [ -z "$display_user" ]
+then
+	echo "WARNING: display owner not found"
+else
+	export XAUTHORITY=/home/$display_user/.Xauthority
+fi
+
+echo "running player"
 lxterminal -e "omxplayer $vid" && xrefresh
